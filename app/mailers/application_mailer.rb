967178a8a02092f62f39896d4da97acf6647a34d
Caio Lopes <caiotuxo@gmail.com>
2018-12-12 16:24:34 -0200

    fix

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -15,7 +15,7 @@ class ApplicationMailer < ActionMailer::Base
     headers 'X-Special-Domain-Specific-Header' => "SecretValue",
             'from' => 'projetos@mobile2you.com.br',
             'sender' => 'projetos@mobile2you.com.br'
-    mail(from: "Equipe Mobile2you <projetos@mobile2you.com.br>", :to => email, :cco => order.package.offer.partner.email :subject => '[Tap2Go] Pedido Realizado')
+    mail(from: "Equipe Mobile2you <projetos@mobile2you.com.br>", :to => email, :cco => order.package.offer.partner.email, :subject => '[Tap2Go] Pedido Realizado')
     
   end
 
