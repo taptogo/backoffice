9a97830cf3b63fa402ba6d0957e194bd033ec2d9
Caio Lopes <caiotuxo@gmail.com>
2018-12-17 10:47:23 -0200

    ajustes

--- app/controllers/webservices/orders_controller.rb
+++ app/controllers/webservices/orders_controller.rb
@@ -7,11 +7,11 @@ class Webservices::OrdersController <  WebservicesController
 
     @order = Order.find(params[:order_id])
 
+
     passJson = '{
     "formatVersion": 1,
     "passTypeIdentifier": "pass.com.Mobile2you.TapToGo",
     "serialNumber": "' + @order.id.to_s + '",
-
     "teamIdentifier" : "A639RLSZU5",
     "organizationName" : "Mobile2you Tecnologia Ltda - ME",
     "description": "Tap2go app",
