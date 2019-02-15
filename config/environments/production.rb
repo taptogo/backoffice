94ff6b4c4b2939972968441c471cf6ca28a52b6b
Caio Lopes <caiotuxo@gmail.com>
2019-02-15 18:28:16 -0200

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,8 +91,8 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
+    address: "imap.securityserver.net",
+    port: 465,
     domain: "taptogo.io",
     authentication: "plain",
     enable_starttls_auto: true,
