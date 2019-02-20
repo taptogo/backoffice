fed3189b578fa415763dd9c15ed749b94f439e89
Caio Lopes <caiotuxo@gmail.com>
2019-02-20 16:44:11 -0300

    cashbackrequest

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,7 @@ Rails.application.configure do
   end
 
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: "taptogo.io" }
+  config.action_mailer.default_url_options = { host: "smtp.office365.com" }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
