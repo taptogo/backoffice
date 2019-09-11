ef3d10117ade09f6b7a1272aad214a6b78844188
Allan Soares Duarte <allan@taptogo.io>
2019-09-11 20:03:29 -0300

    [BAC-1] #comment Added support to finance transactions

--- app/mailers/order_notifier_mailer.rb
+++ app/mailers/order_notifier_mailer.rb
@@ -1,26 +1,38 @@
 class OrderNotifierMailer < ApplicationMailer
     default from: 'Equipe TapToGo <info@taptogo.io>'
 
-    def send_order_email(order)
-        @order = order
-        @name = "Allan"
-        @offer = "Balão"
-        @qtd = "2"
-        @amount = "679.2"
-        @address = "Rua inambu"
-        @date = "17/02/2019"
-        @policy = "Cancelamento em 7 dias"
-        @id = "GYZH12K3L4SE3"
+    def send_order_email(buyer_name, orders, amount, creditCard)
+        @buyer_name = buyer_name
+        @orders     = orders
+        @amount     = amount
+        @creditCard = creditCard
         headers 'X-Special-Domain-Specific-Header' => "SecretValue",
             'from' => 'info@taptogo.io',
             'sender' => 'info@taptogo.io'
         mail(
-            :to => @order,
+            :to => "allan.sduarte@gmail.com",
             :subject => '[TapToGo] Pedido realizado'
         )
     end
 
-    def send_order_to_partner_email
+    def send_order_to_partner_email(
+        partner_name,
+        buyer_name,
+        experience_title,
+        book_date,
+        book_hour,
+        order_price,
+        order_amount,
+        meeting_point
+    )
+    @partner_name       = partner_name
+    @buyer_name         = buyer_name
+    @experience_title   = experience_title
+    @book_date          = book_date
+    @book_hour          = book_hour
+    @order_price        = order_price
+    @order_amount       = order_amount
+    @meeting_point      = meeting_point
         headers 'X-Special-Domain-Specific-Header' => "SecretValue",
             'from' => 'info@taptogo.io',
             'sender' => 'info@taptogo.io'
@@ -30,7 +42,9 @@ class OrderNotifierMailer < ApplicationMailer
         )
     end
 
-    def send_order_to_sale_channel_email
+    def send_order_to_sale_channel_email(experience_title, comission_amount)
+        @experience_title = experience_title
+        @comission_amount =comission_amount
         headers 'X-Special-Domain-Specific-Header' => "SecretValue",
             'from' => 'info@taptogo.io',
             'sender' => 'info@taptogo.io'
