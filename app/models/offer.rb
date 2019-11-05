b3bdcaceacf008cddf0a7ce84e3ae860b5d129cb
Allan Soares Duarte <allan.duarte@gulf.com.br>
2019-11-05 01:08:54 -0300

    [BAC-1] #comment Added price change factor

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -47,6 +47,8 @@ class Offer
 
   field :notes, :type => String
 
+  field :price_change_factor, :type => Float, :default => 0
+
   has_mongoid_attached_file :picture,
     :storage => :s3, 
     :preserve_files => true,
@@ -106,13 +108,6 @@ class Offer
 
   def self.mapOffers (array, user)
     array.map { |u|
-      rawPackage = []
-      packages = Package.availablePackages(u.id.to_s)
-      packages = packages.asc(:date).group_by{|x| x.date}
-      packages.keys.sort.each do |key|
-        rawPackage << {:date => I18n.l(key, :format => "%a-%d %b").gsub("á", "a"), :hours => Package.mapPackages(packages[key]).reject { |i| i[:quantity].blank? || i[:quantity].to_i < 0  }.sort_by { |hsh| hsh[:hour] } }
-      end
-      rawPackage = rawPackage.reject { |i| i[:hours].blank? || i[:hours].count <= 0  }
 
       {
       :id             => u.id.to_s,
@@ -120,13 +115,13 @@ class Offer
       :description    => u.description,
       :price          => u.price,
       :url            => u.url,
-      :liked          => user.nil? ? false : user.favorites.where(:offer_id => u.id.to_s).count > 0,
-      :categories     => Category.mapCategories(u.categories, user),
-      :hashtag        => u.categories.count == 0 ? "" : ("#" + u.categories.first.name.downcase),
-      :likes          => user.nil? ? [] : Favorite.mapFavorites(u.favorites),
+      #:liked          => user.nil? ? false : user.favorites.where(:offer_id => u.id.to_s).count > 0,
+      #:categories     => Category.mapCategories(u.categories, user),
+      #:hashtag        => u.categories.count == 0 ? "" : ("#" + u.categories.first.name.downcase),
+      #:likes          => user.nil? ? [] : Favorite.mapFavorites(u.favorites),
       :picture        => u.picture.url,
-      :policy         => Description.mapDescriptions(Description.availableDescriptions(u.id.to_s), Offer.find(u.id.to_s).policy),
-      :packages       => rawPackage,
+      #:policy         => Description.mapDescriptions(Description.availableDescriptions(u.id.to_s), u.policy),
+      #:packages       => rawPackage,
       :accepts_cash   => u.accepts_cash_transactions,
       :location       => {
         :longitude      => u.longitude,
--- app/models/package.rb
+++ app/models/package.rb
@@ -1,59 +1,60 @@
+# frozen_string_literal: true
+
 class Package
   include Mongoid::Document
 
-  field :enabled, type: Boolean, default: true  
-  field :capacity, type: Integer, default: 0  
-  field :wDay, type: Integer, default: 0  
-  field :total, type: Integer, default: 0  
-  field :price, type: Float, default: 0  
-  field :date, type: Time  
-  field :name, type: String  
-  field :hour, type: String  
+  field :enabled, type: Boolean, default: true
+  field :capacity, type: Integer, default: 0
+  field :wDay, type: Integer, default: 0
+  field :total, type: Integer, default: 0
+  field :price, type: Float, default: 0
+  field :date, type: Time
+  field :name, type: String
+  field :hour, type: String
+  field :price_change_factor, type: Float, default: 0
 
   belongs_to :offer
   before_save :checkName
   has_many :orders, dependent: :destroy
-  scope :availablePackages, -> (offer)  { where(:enabled.ne => false, :offer_id => offer, :date.gt => Time.now, :date.lte => (Time.now + 2.months)).asc(:date) }
-
+  scope :availablePackages, ->(offer) { where(:enabled.ne => false, :offer_id => offer, :date.gt => Time.now, :date.lte => (Time.now + 2.months)).asc(:date) }
 
   def checkName
-      self.name = self.offer.name
-      self.name += " " + (self.date.nil? ? "" : self.date.strftime("%d/%m/%Y %H:%M"))
-      self.total = self.capacity
+    self.name = offer.name
+    self.name += ' ' + (date.nil? ? '' : date.strftime('%d/%m/%Y %H:%M'))
+    self.total = capacity
   end
 
-  def self.mapPackages (array)
-    array.map { |u| {
-     :id => u.id.to_s,
-     :price => u.price,
-     :hour => u.hour,
-     :quantity => u.capacity,
-     :date => I18n.l(u.date, :format => "%a %d %b").gsub("á", "a")
-     }}
+  def self.mapPackages(array)
+    array.map do |u|
+      {
+        id: u.id.to_s,
+        price: u.price,
+        hour: u.hour,
+        quantity: u.capacity,
+        date: I18n.l(u.date, format: '%a %d %b').gsub('á', 'a')
+      }
+    end
   end
 
   def getDate
-    self.date.strftime("%A") + " - " + self.date.strftime("%d/%m/%Y")
+    date.strftime('%A') + ' - ' + date.strftime('%d/%m/%Y')
   end
 
   def getDateFull
-    self.date.strftime("%d/%m/%Y") + " " + self.hour
+    date.strftime('%d/%m/%Y') + ' ' + hour
   end
 
   def getCalendarDateFull
-    begin
-      (self.date.strftime("%d/%m/%Y") + " " + self.hour).to_time
-    rescue
-      (self.date.strftime("%d/%m/%Y") + " 00:00").to_time
-    end
+    (date.strftime('%d/%m/%Y') + ' ' + hour).to_time
+  rescue StandardError
+    (date.strftime('%d/%m/%Y') + ' 00:00').to_time
   end
 
   def getFullName
-    if self.offer.nil?
-      ""
+    if offer.nil?
+      ''
     else
-      self.offer.getFullName
+      offer.getFullName
     end
   end
-
 end
